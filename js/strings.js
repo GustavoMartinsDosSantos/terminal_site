e6fe0aa7b2e15b23e91889d98ffbf6baaa31e4a0
Gustavo Martins dos Santos <guga.m.s8@hotmail.com>
2024-08-20 03:18:21 -0300

    Added fallback to not supported languages

--- js/strings.js
+++ js/strings.js
@@ -1,13 +1,18 @@
-const userLanguage = navigator.language || navigator.userLanguage
+const supportedLanguages = Object.keys(strings);
 
-const strings = {
-    'pt-BR': {
-        commandNotFound: 'Comando \'<?>\' não encontrado',
-        notADirectory: 'cd: \'<?>\': O parâmetro passado não é um diretório'
-    },
-    'en-US': {
-        commandNotFound: 'Command \'<?>\' not found',
-        notADirectory: 'cd: \'<?>\': Not a directory'
-    }
+let userLanguage = navigator.language || navigator.userLanguage;
+
+if (!supportedLanguages.includes(userLanguage)) {
+  userLanguage = "en-US";
 }
 
+const strings = {
+  "pt-BR": {
+    commandNotFound: "Comando '<?>' não encontrado",
+    notADirectory: "cd: '<?>': O parâmetro passado não é um diretório",
+  },
+  "en-US": {
+    commandNotFound: "Command '<?>' not found",
+    notADirectory: "cd: '<?>': Not a directory",
+  },
+};
