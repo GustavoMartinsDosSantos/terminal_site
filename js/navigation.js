4fdaae612d514bd66e0fbadaecee733af65b3332
Gustavo Martins dos Santos <guga.m.s8@hotmail.com>
2024-08-20 05:31:56 -0300

    Updated skills list

--- js/navigation.js
+++ js/navigation.js
@@ -8,8 +8,37 @@ let navigationTree = {
       },
     },
     skills: {
-      "PHP - 2 years": "",
-      "JavaScript - 5 years": "",
+      PHP: {
+        "Laravel - 1 year": "",
+        "Slim - 2 years": "",
+      },
+      JavaScript: {
+        "React - 3 years": "",
+        "NextJs - 2 years": "",
+        "Vue - 1 year": "",
+        "NestJs - 1 year": "",
+        "Google Cloud Functions - 1 year": "",
+        "React Native - 1 year": "",
+      },
+      TypeScript: {
+        "React - 3 years": "",
+        "NextJs - 2 years": "",
+        "Vue - 1 year": "",
+        "NestJs - 1 year": "",
+        "Google Cloud Functions - 1 year": "",
+        "React Native - 1 year": "",
+      },
+      DBs: {
+        "MySQL - 2 years": "",
+        "PostgresSQL - 2 years": "",
+        "Spanner (GCP) - 1 year": "",
+      },
+      Others: {
+        "Docker - 3 years": "",
+        "Linux - 4 years": "",
+        "Google Cloud Platform - 1 year": "",
+        "Unity (C#) - Just a hobby :P": "",
+      }
     },
   },
 };
